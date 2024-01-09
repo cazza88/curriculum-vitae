28313de4e70122bd6fa4ab3de4313bf993afe731
Andrea <andreacazzaniga@outlook.com>
2024-01-09 17:26:31 +0100

    update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -31,18 +31,8 @@ module.exports = {
       company: 'Atlantic Technologies Spa',
       location: 'Milan, IT',
       period: 'Apr 21 - Present',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `Prince Haml
-et is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
-    },
-    {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      skills: ['infor'],
+      contents: ``
     }
   ],
   educations: [
