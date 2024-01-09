90909eb85d8e22e08ed9d0054e2f8f501bff556b
Andrea <andreacazzaniga@outlook.com>
2024-01-09 18:23:51 +0100

    Update metadata.js

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -31,8 +31,101 @@ module.exports = {
       company: 'Atlantic Technologies Spa',
       location: 'Milan, IT',
       period: 'Apr 21 - Present',
-      skills: ['infor'],
+      skills: ['Infor', 'Java', 'TypeScript'],
       contents: ``
+    },
+    {
+      title: 'Full Stack Developer',
+      company: 'Holonix Srl',
+      location: 'Meda, IT',
+      period: 'Jul 18 - Apr 21',
+      skills: ['Java', 'Spring', 'NodeJS', 'VueJS', 'Cassandra', 'Docker', 'IoT'],
+      contents: `
+Full stack developer and system administrator of a propiertary IoT solution platform:
+
+* Developing backend with JAVA / Spring / HIBERNATE
+* Developing frontend/mobile (PWA) with VueJS (Quasar Framework)
+* Developing IoT side with NodeJS (microservices) / Apache Cassandra / RabbitMQ (MQTT, AMQP)
+* DevOps and CI/CD with GitLab
+* System administrator of ubuntu server cluster with Docker / Docker Swarm
+`
+    },
+    {
+      title: 'IT Consultant',
+      company: 'Cicom Soluzioni Informatiche Srl',
+      location: 'Milan, IT',
+      period: 'Jun 15 - Jul 21',
+      skills: ['Infor', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
+      contents: `
+Design and installation of internal infrastructures:
+
+* Server with Windows Server 2012 R2 as DC, ActiveDirectory and GPO configurations
+* Network shares
+* External VPN access
+
+ERP Infor M3 customizations developer:
+
+* ERP customization with proprietary tool Infor MAK (language Java on IDE Eclipse)
+* Creation and edit report with OpenText StreamServe suite
+* Creation of automation processes using Infor proprietary APIs and MEC tools on IDE Eclipse
+
+Internal Resource Management portal maintenance and improvement (language ASP on Visual Studio 2015).
+
+IT Manager and IT Specialist for several medium/big customers:
+
+* System administrator Linux, Windows and VMware
+* Backup systems installation and maintenance
+* Network administrator
+* Email server (Kerio Connect), email archiving (MailStore)
+* ERP maintenance
+* Help desk
+`
+    },
+    {
+      title: 'Software Developer',
+      company: 'Disiware srl',
+      location: 'Cinisello Balsamo, IT',
+      period: 'Nov 12 - Jun 15',
+      //skills: ['Infor', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
+      contents: `
+Development of Biomedical software for processing ATS (Autotransfusion System) machine's data (Sorin Group, Italy).
+
+Development tools used: Microsoft Visual Studio 2010, Microsoft .NET Framework 3.5, WPF (Windows Presentation Foundation) language C#.
+
+Development of Biomedical software for exchanging data between the software and the healthcare standard HL7 (Sorin Group, Germany).
+
+Development tools used: Microsoft Visual Studio 2013, Microsoft .NET Framework 3.5, WPF (Windows Presentation Foundation) language C#.
+`
+    },
+    {
+      title: 'IT Specialist',
+      company: 'NewMediaLabs srl',
+      location: 'Milan, IT',
+      period: 'May 11 - Nov 12',
+      //skills: ['Infor', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
+      contents: `
+* Linux (Debian/CentOS) and Windows (Server 2003, 2008, 2008R2) System Administrator.
+* Network Administrator (firewall, switch and router).
+* Web server management (Apache and IIS).
+* Database management (SQL Server, MySQL with PhpMyAdmin).
+* Physical server installation and maintenance (Xen and VMware hypervisor).
+* Hosting/Housing/VPS Server Management.
+* Email server Management (Exim)
+* Domains Name Registration.
+* Help desk I, II level and relative problem solving.
+`
+    },
+    {
+      title: 'Software Developer',
+      company: 'Disi srl',
+      location: 'Cinisello Balsamo, IT',
+      period: 'Oct 10 - Nov 10',
+      //skills: ['Infor', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
+      contents: `
+University apprenticeship and thesis: "PLUGIN PER INTEGRAZIONE
+'MONITOR CARROZZIERI'"
+Software plugin development in C# and plugin management with WinDev
+`
     }
   ],
   educations: [
@@ -47,5 +140,5 @@ module.exports = {
       company: 'Istituto Magistri Cumacini, Mariano Comense',
     }
 
-  ]    
+  ]
 };
