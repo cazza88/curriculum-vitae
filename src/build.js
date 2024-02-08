b4177513b5a211fc1643f200782d453cc8eb4ea3
Andrea <andreacazzaniga@outlook.com>
2024-02-08 18:18:45 +0100

    add build for IT

--- src/build.js
+++ src/build.js
@@ -1,7 +1,8 @@
 const handlebars = require('handlebars');
 const fs = require('fs-extra');
 const markdownHelper = require('./utils/helpers/markdown');
-const templateData = require('./metadata/metadata');
+const templateDataEN = require('./metadata/metadataEN');
+const templateDataIT = require('./metadata/metadataIT');
 const getSlug = require('speakingurl');
 const dayjs = require('dayjs');
 const repoName = require('git-repo-name');
@@ -9,27 +10,46 @@ const username = require('git-username');
 const buildPdf = require('./utils/pdf.js');
 
 const srcDir = __dirname;
-const outputDir = __dirname + '/../dist';
+const outputDirEN = __dirname + '/../dist/en';
+const outputDirIT = __dirname + '/../dist/it';
 
 // Clear dist dir
-fs.emptyDirSync(outputDir);
+fs.emptyDirSync(outputDirEN);
+fs.emptyDirSync(outputDirIT);
 
 // Copy assets
-fs.copySync(srcDir + '/assets', outputDir);
+fs.copySync(srcDir + '/assets', outputDirEN);
+fs.copySync(srcDir + '/assets', outputDirIT);
 
 // Build HTML
 handlebars.registerHelper('markdown', markdownHelper);
 const source = fs.readFileSync(srcDir + '/templates/index.html', 'utf-8');
 const template = handlebars.compile(source);
-const pdfFileName = `${getSlug(templateData.name)}.pdf`;
-const html = template({
-  ...templateData,
-  baseUrl: `https://${username()}.github.io/${repoName.sync()}`,
+var pdfFileName = `${getSlug(templateDataEN.name)}.pdf`;
+const htmlEN = template({
+  ...templateDataEN,
+  baseUrl: `https://${username()}.github.io/${repoName.sync()}/en`,
+  otherUrl: `https://${username()}.github.io/${repoName.sync()}/it`,
   pdfFileName,
   updated: dayjs().format('MMMM D, YYYY'),
 });
 
-fs.writeFileSync(outputDir + '/index.html', html);
+pdfFileName = `${getSlug(templateDataIT.name)}.pdf`;
+
+fs.writeFileSync(outputDirEN + '/index.html', htmlEN);
 
 // Build PDF
-buildPdf(`${outputDir}/index.html`, `${outputDir}/${pdfFileName}`);
+buildPdf(`${outputDirEN}/index.html`, `${outputDirEN}/${pdfFileName}`);
+
+
+const htmlIT = template({
+  ...templateDataIT,
+  baseUrl: `https://${username()}.github.io/${repoName.sync()}/it`,
+  otherUrl: `https://${username()}.github.io/${repoName.sync()}/en`,
+  pdfFileName,
+  updated: dayjs().format('DD/MM/YYYY'),
+});
+
+fs.writeFileSync(outputDirIT + '/index.html', htmlIT);
+
+buildPdf(`${outputDirIT}/index.html`, `${outputDirIT}/${pdfFileName}`);
