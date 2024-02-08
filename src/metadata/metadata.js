fd673a698ad624a4b0a47dda57aec94554c0fe78
Andrea <andreacazzaniga@outlook.com>
2024-02-08 15:34:25 +0100

    update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -11,120 +11,114 @@ module.exports = {
     'Mobile': '<a href="tel:+393495240151"><i class="fa fa-phone fact-icon"></i>+39 3495240151</a>',
     'Email': '<a href="mailto:andreacazzaniga@outlook.com"><i class="fa fa-envelope fact-icon"></i>andreacazzaniga@outlook.com</a>'
   },
-  skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 60],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 80],
-    ['Literature', 100],
-    ['Influence', 100],
-  ],
   positions: [
     {
-      title: 'Infor Technical Consultant',
+      title: 'Technical Consultant',
       company: 'Atlantic Technologies Spa',
       location: 'Milan, IT',
-      period: 'Apr 21 - Present',
+      period: 'Apr 2021 - Present',
       skills: ['Infor', 'Java', 'TypeScript'],
-      contents: ``
+      contents: `
+Infor M3 technical consultant:
+
+* Developing frontend H5 Apps with Infor H5 SDK (Angular, Typescript) and H5 Scripts (Typescript)
+* Developing external interfaces with Java / NodeJS (microservices) / Docker
+* Process automation with Infor ION APIs (REST), MEC tools, ION dataflow / workflow
+* Developing custom report with OpenText StreamServe / OpenText ExStream / IDM
+
+Also technical lead in some projects
+`
     },
     {
       title: 'Full Stack Developer',
       company: 'Holonix Srl',
       location: 'Meda, IT',
-      period: 'Jul 18 - Apr 21',
-      skills: ['Java', 'Spring', 'NodeJS', 'VueJS', 'Cassandra', 'Docker', 'IoT'],
+      period: 'Jul 2018 - Apr 2021',
+      skills: ['Java', 'Spring', 'NodeJS', 'VueJS', 'Cassandra', 'Docker', 'IoT', 'Linux'],
       contents: `
-Full stack developer and system administrator of a propiertary IoT solution platform:
+Improved the developer experience as a full stack developer and system administrator of a propiertary IoT solution platform:
 
 * Developing backend with JAVA / Spring / HIBERNATE
 * Developing frontend/mobile (PWA) with VueJS (Quasar Framework)
 * Developing IoT side with NodeJS (microservices) / Apache Cassandra / RabbitMQ (MQTT, AMQP)
 * DevOps and CI/CD with GitLab
 * System administrator of ubuntu server cluster with Docker / Docker Swarm
+
+Participation in EU projects: bIoTope, Lincoln, Z-Fact0r and Z-BRE4K
 `
     },
     {
-      title: 'IT Consultant',
+      title: 'Technical Consultant',
       company: 'Cicom Soluzioni Informatiche Srl',
       location: 'Milan, IT',
-      period: 'Jun 15 - Jul 21',
-      skills: ['Infor', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
+      period: 'Jun 2015 - Jul 2018',
+      skills: ['Infor', 'M3', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
       contents: `
-Design and installation of internal infrastructures:
+Started as internal system administrator / developer:
 
-* Server with Windows Server 2012 R2 as DC, ActiveDirectory and GPO configurations
-* Network shares
-* External VPN access
+* Windows Server 2012 R2 as DC / ActiveDirectory / GPO / Network shares
+* External VPN
+* Developing frontend of Resource Management portal with ASP.
 
-ERP Infor M3 customizations developer:
+Later Infor M3 technical consultant:
 
-* ERP customization with proprietary tool Infor MAK (language Java on IDE Eclipse)
-* Creation and edit report with OpenText StreamServe suite
-* Creation of automation processes using Infor proprietary APIs and MEC tools on IDE Eclipse
+* Developing ERP customization with Java (Infor MAK tool)
+* Process automation with Infor ION APIs (REST) and MEC tools
+* Developing custom report with OpenText StreamServe suite
 
-Internal Resource Management portal maintenance and improvement (language ASP on Visual Studio 2015).
+System administrator for several medium/big customers:
 
-IT Manager and IT Specialist for several medium/big customers:
-
-* System administrator Linux, Windows and VMware
-* Backup systems installation and maintenance
-* Network administrator
+* OS (Linux, Windows), hypervisor (VMware ESXi), network, backup systems
 * Email server (Kerio Connect), email archiving (MailStore)
 * ERP maintenance
+* Qlik BI
 * Help desk
 `
     },
     {
       title: 'Software Developer',
-      company: 'Disiware srl',
+      company: 'Disiware Srl',
       location: 'Cinisello Balsamo, IT',
-      period: 'Nov 12 - Jun 15',
-      //skills: ['Infor', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
+      period: 'Nov 2012 - Jun 2015',
+      skills: ['C#', 'WPF', '.NET', 'Visual Studio', 'Microsoft'],
       contents: `
-Development of Biomedical software for processing ATS (Autotransfusion System) machine's data (Sorin Group, Italy).
+Software developer for a biomedical costumer (Sorin Group):
 
-Development tools used: Microsoft Visual Studio 2010, Microsoft .NET Framework 3.5, WPF (Windows Presentation Foundation) language C#.
-
-Development of Biomedical software for exchanging data between the software and the healthcare standard HL7 (Sorin Group, Germany).
-
-Development tools used: Microsoft Visual Studio 2013, Microsoft .NET Framework 3.5, WPF (Windows Presentation Foundation) language C#.
+* Developing software for processing ATS (Autotransfusion System) machine's data with C# (.NET Framework 3.5, WPF, Visual Studio 2010)
+* Developing software for exchanging data between the software and the healthcare standard HL7 with C# (.NET Framework 3.5, WPF, Visual Studio 2013)
 `
     },
     {
-      title: 'IT Specialist',
-      company: 'NewMediaLabs srl',
+      title: 'System Administrator',
+      company: 'NewMediaLabs Srl',
       location: 'Milan, IT',
-      period: 'May 11 - Nov 12',
-      //skills: ['Infor', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
+      period: 'May 2011 - Nov 2012',
+      skills: ['SysAdmin', 'Linux', 'Windows Server', 'VMware', "Apache"],
       contents: `
-* Linux (Debian/CentOS) and Windows (Server 2003, 2008, 2008R2) System Administrator.
-* Network Administrator (firewall, switch and router).
-* Web server management (Apache and IIS).
-* Database management (SQL Server, MySQL with PhpMyAdmin).
-* Physical server installation and maintenance (Xen and VMware hypervisor).
-* Hosting/Housing/VPS Server Management.
-* Email server Management (Exim)
-* Domains Name Registration.
-* Help desk I, II level and relative problem solving.
+System administrator of a web hosting infrastructure:
+
+* OS Linux (Debian, CentOS) and Windows Server (2003, 2008, 2008R2) 
+* Network (firewall, switch, router)
+* Web server (Apache, IIS)
+* Database (SQL Server, MySQL)
+* Hypervisor (Xen, VMware ESXi)
+* Email server (Exim)
+* Hosting/Housing/VPS server
+
+Help desk I, II level and relative problem solving.
 `
     },
     {
-      title: 'Software Developer',
-      company: 'Disi srl',
+      title: 'Software Developsr',
+      company: 'Disi Srl',
       location: 'Cinisello Balsamo, IT',
-      period: 'Oct 10 - Nov 10',
-      //skills: ['Infor', 'Java', 'ASP', 'Linux', 'Windows', 'VMware'],
+      period: 'Oct 2010 - Nov 2010',
+      skills: ['C#', '.NET', 'WinDev'],
       contents: `
-University apprenticeship and thesis: "PLUGIN PER INTEGRAZIONE
-'MONITOR CARROZZIERI'"
-Software plugin development in C# and plugin management with WinDev
+University apprenticeship and thesis: "PLUGIN PER INTEGRAZIONE 'MONITOR CARROZZIERI'" 
+
+* Developing software plugin with C# 
+* Plugin management with WinDev
 `
     }
   ],
